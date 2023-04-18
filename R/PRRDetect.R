f1c96817510542f755e2af24bb75f7865402dd36
GRinaldi97 <giuseppe.rinaldi1497@gmail.com>
2023-04-18 14:05:21 +0100

    First functioning version

--- R/PRRDetect.R
+++ R/PRRDetect.R
@@ -1,16 +1,19 @@
-prepare_PRRDetect <- function(Indel_VCF_path, SNV_VCF_path, genome.v, nparallel, is.filtered_SNV=F, is.filtered_InDel=F, sample_name){
+prepare_PRRDetect <- function(Indel_VCF_path, SNV_VCF_path, genome.v, nparallel, is.filtered_SNV=F, is.filtered_InDel=F, sample_name, organ, setseed = 42){
   if(is.filtered_SNV == F){
       SNV_Filter(SNV_VCF_path, vcfout = paste0(SNV_VCF_path, ".filtered"), genomev = genome.v,sample_name = sample_name )
-      SNV_VCF_path <- paste0(SNV_VCF_path, ".filtered")
+      SNV_VCF_path <- paste0(SNV_VCF_path, ".filtered.bgz")
    }
   if(is.filtered_InDel == F){
     InDelFilter(vcffile = Indel_VCF_path, vcfout = paste0(Indel_VCF_path, ".filtered"), genomev = genome.v, sample_name = sample_name )
-    Indel_VCF_path <- paste0(Indel_VCF_path, ".filtered")
+    Indel_VCF_path <- paste0(Indel_VCF_path, ".filtered.bgz")
    }
 
   catalogs  <- generate_catalogs_from_vcf(Indel_VCF_path, SNV_VCF_path, genome.v, sample_name)
-  signature_fits <- Signature_fit(catalogs$SNV_catalog, catalogs$Indel_Catalog, organ, nparallel)
-  return(signature_fits)
+  signature_fits <- Signature_fit(SNV_Catalog = catalogs$SNV_Catalog, InDel_Catalog = catalogs$Indel_Catalog, organ, nparallel, setseed)
+  SNV_fit <- signature_fits$SNV$exposures
+  InDel_fit <- signature_fits$InDel$exposures
+  colnames(SNV_fit) <- unlist(lapply(strsplit(colnames(SNV_fit), split = "_", fixed = T), function(x){tail(x,1)}))
+  return(list("SNV"=SNV_fit, "InDel"=InDel_fit))
 }
 
 
@@ -36,10 +39,37 @@ PRRDetect <- function(InDel_fits, SNV_fits){
   pure_POL_indS <- paste0("RefSig.InD", c(14,15))
   mixed_indS <- paste0("RefSig.InD", c("16a", "16b", 20, 21))
 
-  #############################################
+  ## if the name of the signature is not present, which returns "integer(0)", then the sum is equal to 0, otherwise is equal to the rowsum
 
-  ## add prediction and and label
+  ## SNV
+  ifelse(identical(which(colnames(SNV_fits) %in% pure_MMR_sigs), integer(0)), MMR_SBS <- 0 , MMR_SBS <- rowSums(SNV_fits[,which(colnames(SNV_fits) %in% pure_MMR_sigs), drop=F]))
 
-  #############################################
-  return(sample_out)
+  ifelse(identical(which(colnames(SNV_fits) %in% pure_POL_sigs), integer(0)), POL_SBS <- 0 , POL_SBS <- rowSums(SNV_fits[,which(colnames(SNV_fits) %in% pure_POL_sigs), drop=F]))
+
+  ifelse(identical(which(colnames(SNV_fits) %in% mixed_sigs), integer(0)), MIX_SBS <- 0 , MIX_SBS <- rowSums(SNV_fits[,which(colnames(SNV_fits) %in% mixed_sigs), drop=F]))
+
+
+  ## INDEL
+  ifelse(identical(which(colnames(InDel_fits) %in% pure_MMR_sigs), integer(0)), MMR_IND <- 0 , MMR_IND <- rowSums(InDel_fits[,which(colnames(InDel_fits) %in% pure_MMR_indS), drop=F]))
+
+  ifelse(identical(which(colnames(InDel_fits) %in% pure_POL_sigs), integer(0)), POL_IND <- 0 , POL_IND <- rowSums(InDel_fits[,which(colnames(InDel_fits) %in% pure_POL_indS), drop=F]))
+
+  ifelse(identical(which(colnames(InDel_fits) %in% mixed_sigs), integer(0)), MIX_IND <- 0 , MIX_IND <- rowSums(InDel_fits[,which(colnames(InDel_fits) %in% mixed_indS), drop=F]))
+
+
+  Ratio <- (rowSums(InDel_fits[,-which(colnames(InDel_fits)=="unassigned"), drop = F]) + 1) / (rowSums(SNV_fits[,-which(colnames(SNV_fits) == "unassigned"), drop = F]) + 1)
+
+  final_df <- as.data.frame(cbind(MMR_SBS, MIX_SBS, POL_SBS, MMR_IND, MIX_IND, POL_IND, Ratio))
+
+  final_df[,1:6] <- log2(final_df[,1:6]+1)
+  final_df[,7] <- log2(final_df[,7])
+
+  final_df <- prediction_function(final_df)
+  colnames(final_df)[1:4] <- c("MMRd", "MMRd+Poly-dys", "Negative", "Poly-dys")
+  final_df$Prediction = prediction_label(final_df)
+  final_df <- final_df[,c(3,1,2,4,5)]
+  return(final_df)
+
 }
+
+
--- R/signature_fit.R
+++ R/signature_fit.R
@@ -1,8 +1,6 @@
-Signature_fit <- function(SNV_Catalog, InDel_Catalog, organ, nparallel){
-
+Signature_fit <- function(SNV_Catalog, InDel_Catalog, organ, nparallel, setseed){
+  set.seed(setseed)
   SNV_fit <- signature.tools.lib::FitMS(catalogues = SNV_Catalog, organ = organ,useBootstrap = T, nparallel = nparallel )
-
   InDel_fit <- signature.tools.lib::Fit(catalogues = InDel_Catalog, signatures = read.table("data/InDel_Consensus_Sigs.tsv", sep = "\t"), useBootstrap = T, nparallel= nparallel)
-
   return(list("SNV"=SNV_fit, "InDel"=InDel_fit))
 }
